c755bf5be01fff84a1594a667511bc5679646407
dylanfisher <hi@dylanfisher.com>
2017-08-28 22:42:48 -0400

    Home page and nav layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(function() {
-  console.log('main.js -> it works');
+App = {};
+
+$(document).on('click', '.hamburger-wrapper', function() {
+  if ( App.navIsOpen() ) {
+    App.closeNav();
+  } else {
+    App.openNav();
+  }
 });
+
+App.navIsOpen = function() {
+  return $('html').hasClass('showing-nav');
+};
+
+App.openNav = function() {
+  $('html').addClass('showing-nav');
+};
+
+App.closeNav = function() {
+  $('html').removeClass('showing-nav');
+};
